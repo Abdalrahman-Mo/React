4ade23e2c7ce4ddfa818ab547375fde3cf3ac166
Abdalrahman Mo <abdalrahmanm170@gmail.com>
2025-01-08 17:01:38 +0200

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,5 @@
 function App() {
-  return (
-    <>
-      <h1>Hello world!</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet
-        recusandae velit, veritatis facere error optio beatae tempore corporis
-        architecto, ea esse asperiores ipsam maxime ullam quis tempora vero,
-        earum id.
-      </p>
-      <button>send</button>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
