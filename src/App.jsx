b6e512bbbbaadeac2424a2959851cd624637bdca
Abdalrahman Mo <abdalrahmanm170@gmail.com>
2025-01-08 16:55:09 +0200

    v1 of the app for components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,13 @@ function App() {
   return (
     <>
       <h1>Hello world!</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet
+        recusandae velit, veritatis facere error optio beatae tempore corporis
+        architecto, ea esse asperiores ipsam maxime ullam quis tempora vero,
+        earum id.
+      </p>
+      <button>send</button>
     </>
   );
 }
