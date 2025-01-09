8c2cedca46bf9867c60231c43fe61c0ded840916
Abdalrahman Mo <abdalrahmanm170@gmail.com>
2025-01-09 20:39:02 +0200

    bootstrap added

--- src/components/Employee.jsx
+++ src/components/Employee.jsx
@@ -1,7 +1,21 @@
+import Popup from "./Popup";
 export default function Employee(props) {
   return (
-    <div>
-      Name :{props.name} ---------- Role: {props.role}
-    </div>
+    <>
+      <div className="py-8 px-8 max-w-sm mx-auto space-y-2 bg-white rounded-xl shadow-lg sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:gap-x-6">
+        <img
+          className="block object-cover w-[80px] h-[80px] mx-auto h-24 sm:mx-0 sm:shrink-0 rounded-full"
+          src={props.img}
+          alt="Woman's Face"
+        />
+        <div className="text-center space-y-2 sm:text-left">
+          <div className="space-y-0.5">
+            <p className="text-lg text-black font-semibold">{props.name}</p>
+            <p className="text-slate-500 font-medium">{props.role}</p>
+          </div>
+          <Popup />
+        </div>
+      </div>
+    </>
   );
 }
